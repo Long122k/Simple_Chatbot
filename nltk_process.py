edb259bb722cfc3bdb111f3e6d440d51eb2f19a1
Long122k <longdo8000@gmail.com>
2021-07-21 16:50:24 +0700

    update Chatbot dataset

--- nltk_process.py
+++ nltk_process.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 # nltk.download('punkt')
 
 def tokenize(sentence):
@@ -8,11 +9,15 @@ def tokenize(sentence):
 def stem(word):
     return PorterStemmer().stem(word.lower())
 
-def bag_words(tokenize_sentences, words):
-    return 0
-
-sen = "Hello how are you?"
-list_sen = ["interested", "insteresting"]
-
-# print(tokenize(sen))
-print([stem(i) for i in list_sen])
+def bag_words(tokenized_sentence, words):
+    """
+    sentence = ["Good", "morning"]
+    words = ["hello", "hi", "good", "thank", "you", "morning"]
+    bag   = [   0,      0,      1,      0,      0,      1    ]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(words), dtype= np.float32)
+    for idx,word in enumerate(words):
+        if word in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
--- train.py
+++ train.py
@@ -1,5 +1,11 @@
-from nltk_process import tokenize, stem
+from nltk.util import transitive_closure
+from nltk_process import bag_words, tokenize, stem
 import json
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 with open('intents.json') as f:
     intents = json.load(f)
@@ -21,4 +27,36 @@ words = [stem(word) for word in words if word not in ignore_word]
 words = sorted(set(words))
 tags = sorted(set(tags))
 
-print(words)
+
+#set data train
+X_train = []
+Y_train = []
+
+for (pattern_sentence, tag) in tag_word:
+    bag = bag_words(pattern_sentence, words)
+    X_train.append(bag)
+    label = tags.index(tag)
+    Y_train.append(label)#cross entropy
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = Y_train
+
+    #dataset[idx]
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
+
+# hyperparameters
+batch_size = 8
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+print(X_train)
